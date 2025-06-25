ea2dfdca573d6c2a206b5dabc94ec62f10a6f698
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 19:23:08 +0500

    Update urls.py

--- BeautyCity/urls.py
+++ BeautyCity/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from BeautyCity import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppHome.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
