fc2dd80143ca8fb18865e5fae870ac128ae9c737
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 21:09:32 +0500

    Update admin.py

--- AppService/admin.py
+++ AppService/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Master
+from .models import Master, Salon
 
 
 @admin.register(Master)
 class MasterAdmin(admin.ModelAdmin):
 	list_display = ('first_name', 'last_name', 'speciality',)
 	list_filter = ('speciality',)
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+	list_display = ('name', 'address')
