651a14150e78b70d0a323c9d7684ffd3db517b68
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 17:36:26 +0500

    Добавлен клиент

--- AppHome/admin.py
+++ AppHome/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Client
 
 
-
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+	list_display = ('id', 'first_name', 'last_name', 'phone', 'created_at',)
+	list_filter = ('created_at',)
+	readonly_fields = ('created_at',)
