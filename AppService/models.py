c9608cb4fa78032ee2b209814bcb493dccfb11f0
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 22:07:12 +0500

    Добавлена модель услуги

--- AppService/models.py
+++ AppService/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -54,3 +54,41 @@ class Salon(models.Model):
 
 	def __str__(self):
 		return f'{self.name}'
+
+
+class Service(models.Model):
+	SERVICES = [
+		('hair_coloring', 'окрашивание волос'),
+		('hair_styling', 'укладка волос'),
+		('manicure_сlassic', 'Маникюр классический'),
+		('pedicure', 'педикюр'),
+		('nail_extensions', 'наращивание ногтей'),
+		('daytime_makeup', 'дневной макияж'),
+		('wedding_makeup', 'свадебный макияж'),
+		('evening_makeup', 'вечерний макияж'),
+	]
+
+	name = models.CharField(
+		choices=SERVICES,
+		max_length=100,
+		verbose_name='Услуга',
+	)
+	price = models.DecimalField(
+		max_digits=10,
+		decimal_places=2,
+		validators=[MinValueValidator(1)],
+		verbose_name='Цена',
+	)
+	image = models.ImageField(
+		upload_to='services/',
+		null=True,
+		blank=True,
+		verbose_name='Фото салона',
+	)
+
+	class Meta:
+		verbose_name = 'Услугу'
+		verbose_name_plural = 'Услуги'
+
+	def __str__(self):
+		return self.get_name_display()
