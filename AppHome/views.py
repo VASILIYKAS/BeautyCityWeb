189e59f654ce1f144cd81a08bb571487fe321c4e
VASILIYKAS <yoyoynv@gmail.com>
2025-06-25 12:42:00 +0500

    Подключил статику

--- AppHome/views.py
+++ AppHome/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def notes(request):
+    return render(request, 'notes.html')
+
+
+def popup(request):
+    return render(request, 'popup.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def service_finally(request):
+    return render(request, 'serviceFinally.html')
