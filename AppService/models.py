33346cad7e66d014b84b2decb40516dde15619c4
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 19:22:40 +0500

    Create models.py

---
+++ AppService/models.py
@@ -0,0 +1,38 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+
+class Master(models.Model):
+	CHOICE_SPECIALIZATION = [
+		('stylist', 'стилист'),
+		('make-up', 'визажист'),
+		('manicurist', 'мастер маникюра'),
+		('barber', 'парикмахер'),
+	]
+
+	first_name = models.CharField(max_length=100, verbose_name='Имя')
+	last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+	speciality = models.CharField(
+		choices=CHOICE_SPECIALIZATION,
+		max_length=20,
+		verbose_name='Специальность',
+	)
+	experience_years = models.PositiveIntegerField(default=0, verbose_name='Стаж лет')
+	experience_monts = models.PositiveIntegerField(
+		default=0,
+		validators=[MaxValueValidator(11)],
+		verbose_name='Стаж месяцев',
+	)
+	image = models.ImageField(
+		upload_to='masters_photos/',
+		null=True,
+		blank=True,
+		verbose_name='Фото мастера',
+	)
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name} ({self.get_speciality_display()})'
+
+	class Meta:
+		verbose_name = 'Мастера'
+		verbose_name_plural = 'Мастера'
