8ed7e709d44e4efb0f30e9bedd8bd1e6b7af3905
Aleksashka301 <aleksashka301@gmail.com>
2025-06-25 19:22:19 +0500

    Create admin.py

---
+++ AppService/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Master
+
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+	list_display = ('first_name', 'last_name', 'speciality',)
+	list_filter = ('speciality',)
