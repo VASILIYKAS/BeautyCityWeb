a31ff8358a1a5b9fb1707898c95498c73d61e229
Aleksashka301 <aleksashka301@gmail.com>
2025-06-26 21:38:57 +0500

    Добавлена модель Записи

--- AppService/models.py
+++ AppService/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from AppHome.models import Client
+
 
 class Master(models.Model):
 	CHOICE_SPECIALIZATION = [
@@ -92,3 +94,80 @@ class Service(models.Model):
 
 	def __str__(self):
 		return self.get_name_display()
+
+
+class Appointment(models.Model):
+	TIME_SLOTS = [
+		('10:00', '10:00'),
+		('10:30', '10:30'),
+		('12:00', '12:00'),
+		('12:30', '12:30'),
+		('15:00', '15:00'),
+		('16:30', '16:30'),
+		('17:00', '17:00'),
+		('18:30', '18:30'),
+		('19:00', '19:00'),
+	]
+	PAYMENT_STATUS = [
+		('paid', 'оплачено'),
+		('not_paid', 'не оплачено')
+	]
+
+	client = models.ForeignKey(
+		Client,
+		on_delete=models.CASCADE,
+		related_name='client_appointments',
+		verbose_name='Клиент'
+	)
+	salon = models.ForeignKey(
+		Salon,
+		on_delete=models.CASCADE,
+		related_name='salon_appointments',
+		verbose_name='Салон',
+	)
+	service = models.ForeignKey(
+		Service,
+		on_delete=models.CASCADE,
+		related_name='service_appointments',
+		verbose_name='Услуга'
+	)
+	master = models.ForeignKey(
+		Master,
+		on_delete=models.CASCADE,
+		related_name='master',
+		verbose_name='Мастер',
+	)
+	price = models.DecimalField(
+		max_digits=8,
+		decimal_places=2,
+		validators=[MinValueValidator(1)],
+		verbose_name='Цена',
+	)
+	date = models.DateField(verbose_name='Дата приёма')
+	reception_time = models.CharField(
+		choices=TIME_SLOTS,
+		max_length=5,
+		verbose_name='Время приёма',
+	)
+	status = models.CharField(
+		choices=PAYMENT_STATUS,
+		default='not_paid',
+		max_length=20,
+		verbose_name='Оплата'
+	)
+	created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания заявки')
+
+	@classmethod
+	def get_slot_employment(cls, master, date, reception_time):
+		return cls.objects.filter(
+			master=master,
+			date=date,
+			reception_time=reception_time
+		).exists()
+
+	class Meta:
+		verbose_name = 'Запись'
+		verbose_name_plural = 'Записи'
+
+	def __str__(self):
+		return f'Запись №{self.id}'
