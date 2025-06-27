44be3933578d6c4adbe262d592dcf052651a60ff
VASILIYKAS <yoyoynv@gmail.com>
2025-06-27 12:20:49 +0500

    Добавлено отображени салов на главной странице из БД

--- AppHome/views.py
+++ AppHome/views.py
@@ -1,36 +1,40 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-
+from AppService.models import Salon
 from AppHome.forms import ClientRegistrationForm
 
 
 def index(request):
-	return render(request, 'index.html')
+    salons = Salon.objects.all()
+    context = {'salons': salons}
+
+    return render(request, 'index.html', context)
 
 
 def notes(request):
-	return render(request, 'notes.html')
+    return render(request, 'notes.html')
 
 
 def popup(request):
-	return render(request, 'popup.html')
+    return render(request, 'popup.html')
 
 
 def service(request):
-	return render(request, 'service.html')
+    return render(request, 'service.html')
 
 
 def service_finally(request):
-	return render(request, 'serviceFinally.html')
+    return render(request, 'serviceFinally.html')
+
 
 def register(request):
-	if request.method == 'POST':
-		form = ClientRegistrationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			return redirect('home')
-	else:
-		form = ClientRegistrationForm()
+    if request.method == 'POST':
+        form = ClientRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = ClientRegistrationForm()
 
-	return render(request, 'registration/register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
