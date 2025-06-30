1e56752ddbe9559e99d35653ed60d262fc0969d2
VASILIYKAS <yoyoynv@gmail.com>
2025-06-30 10:42:42 +0500

    Добавлены мастера в шаблон

--- AppService/views.py
+++ AppService/views.py
@@ -4,28 +4,30 @@ from .models import Salon, Service, Master
 
 
 def get_groups_services():
-	services = Service.objects.all()
-	grouped_services = {}
+    services = Service.objects.all()
+    grouped_services = {}
 
-	for service in services:
-		group_name = service.get_group_services_display()
-		if group_name not in grouped_services:
-			grouped_services[group_name] = []
+    for service in services:
+        group_name = service.get_group_services_display()
+        if group_name not in grouped_services:
+            grouped_services[group_name] = []
 
-		grouped_services[group_name].append({
-			'name': service.name,
-			'price': service.price
-		})
+        grouped_services[group_name].append({
+            'name': service.name,
+            'price': service.price
+        })
 
-	return grouped_services
+    return grouped_services
 
 
 def page_service(request):
-	salons = Salon.objects.all()
+    salons = Salon.objects.all()
+    masters = Master.objects.all()
 
-	context = {
-		'salons': salons,
-		'services': get_groups_services()
-	}
+    context = {
+        'salons': salons,
+        'services': get_groups_services(),
+        'masters': masters,
+    }
 
-	return render(request, 'service.html', {'context': context})
+    return render(request, 'service.html', {'context': context})
